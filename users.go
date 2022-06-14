43fce632abc75cbc08944ff93ea1d323f9fb93ec
WiaanB <whoppingwaki@gmail.com>
2022-06-14 22:38:15 +0200

    fetching for IDs on users

--- users.go
+++ users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func handleUsers(w http.ResponseWriter, req *http.Request) {
@@ -25,6 +26,22 @@ func usersGET(body interface{}) {
 	m, test := interfaceToMap(body)
 	if test {
 		ErrorLogger.Println("Failed to convert JSON body")
+	}
+
+	if id, ok := m["id"]; ok {
+		stmnt := fmt.Sprintf("SELECT * FROM users WHERE id = %d;", int(id.(float64)))
+		fmt.Println(stmnt)
+	} else if ids, ok := m["ids"]; ok {
+		var s []string
+		for _, v := range ids.([]interface{}) {
+			s = append(s, fmt.Sprintf("%d", int(v.(float64))))
+		}
+		arrStr := strings.Join(s, ",")
+		stmnt := fmt.Sprintf("SELECT * FROM users WHERE id IN (%s);", arrStr)
+		fmt.Println(stmnt)
+	} else {
+		stmnt := "SELECT * FROM users;"
+		fmt.Println(stmnt)
 	}
 }
 
