082331490da799e6c0480cb361be930f2928bc03
WiaanB <whoppingwaki@gmail.com>
2022-06-15 22:04:07 +0200

    retrieved db values, wip to return JSON response

--- users.go
+++ users.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 )
+
+type User struct {
+	id      int    `json:"id"`
+	name    string `json:"name"`
+	surname string `json:"surname"`
+	age     int    `json:"age"`
+}
 
 func handleUsers(w http.ResponseWriter, req *http.Request) {
 	// Receiving the body to pass along
@@ -12,7 +20,14 @@ func handleUsers(w http.ResponseWriter, req *http.Request) {
 	// Handle the types of requests
 	switch req.Method {
 	case "GET":
-		usersGET(body)
+		response := usersGET(body)
+		fmt.Println(response)
+		w.Header().Set("Content-Type", "application/json")
+		j, err := json.Marshal(response)
+		if err != nil {
+			ErrorLogger.Println("Failed to convert JSON body")
+		}
+		w.Write(j)
 	case "POST":
 		usersPOST()
 	case "PUT":
@@ -22,14 +37,15 @@ func handleUsers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func usersGET(body interface{}) {
+func usersGET(body interface{}) []interface{} {
 	m, test := interfaceToMap(body)
 	if test {
 		ErrorLogger.Println("Failed to convert JSON body")
 	}
 
+	var stmnt string
 	if id, ok := m["id"]; ok {
-		stmnt := fmt.Sprintf("SELECT * FROM users WHERE id = %d;", int(id.(float64)))
+		stmnt = fmt.Sprintf("SELECT * FROM users WHERE id = %d;", int(id.(float64)))
 		fmt.Println(stmnt)
 	} else if ids, ok := m["ids"]; ok {
 		var s []string
@@ -37,12 +53,26 @@ func usersGET(body interface{}) {
 			s = append(s, fmt.Sprintf("%d", int(v.(float64))))
 		}
 		arrStr := strings.Join(s, ",")
-		stmnt := fmt.Sprintf("SELECT * FROM users WHERE id IN (%s);", arrStr)
+		stmnt = fmt.Sprintf("SELECT * FROM users WHERE id IN (%s);", arrStr)
 		fmt.Println(stmnt)
 	} else {
-		stmnt := "SELECT * FROM users;"
+		stmnt = "SELECT * FROM users;"
 		fmt.Println(stmnt)
 	}
+	rows, err := DB.Query(stmnt)
+	var resp []interface{}
+	for rows.Next() {
+		var r User
+		err = rows.Scan(&r.id, &r.name, &r.surname, &r.age)
+		if err != nil {
+			ErrorLogger.Println("Failed to read DB response")
+		}
+		var inInterface map[string]interface{}
+		inrec, _ := json.Marshal(r)
+		json.Unmarshal(inrec, &inInterface)
+		resp = append(resp, inInterface)
+	}
+	return resp
 }
 
 func usersPOST() {
