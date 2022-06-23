71ef4e3283d176eef12a88051dd86a1f36f5e71a
WiaanB <whoppingwaki@gmail.com>
2022-06-23 23:28:13 +0200

    added comments to the usersGET function

--- users.go
+++ users.go
@@ -36,15 +36,20 @@ func handleUsers(w http.ResponseWriter, req *http.Request) {
 }
 
 func usersGET(body interface{}) []interface{} {
+
+	// Ensure a map[string]interface{} to handle the retrieval of users.
 	m, test := interfaceToMap(body)
 	if test {
 		ErrorLogger.Println("Failed to convert JSON body")
 	}
-
+	// Prepare the query string.
 	var stmnt string
 	if id, ok := m["id"]; ok {
+		// Single ID GET.
 		stmnt = fmt.Sprintf("SELECT * FROM users WHERE id = %d;", int(id.(float64)))
 	} else if ids, ok := m["ids"]; ok {
+		// Multiple IDs query.
+		// Format the slice, into a joined string that can be used in the query.
 		var s []string
 		for _, v := range ids.([]interface{}) {
 			s = append(s, fmt.Sprintf("%d", int(v.(float64))))
@@ -52,15 +57,19 @@ func usersGET(body interface{}) []interface{} {
 		arrStr := strings.Join(s, ",")
 		stmnt = fmt.Sprintf("SELECT * FROM users WHERE id IN (%s);", arrStr)
 	} else {
+		// Base select all users.
 		stmnt = "SELECT * FROM users;"
 	}
+	// Query the DB to fetch the data.
 	rows, err := DB.Query(stmnt)
 	errorHandle(err, "Failed to query DB")
 	var resp []interface{}
+	// Loop over all the rows.
 	for rows.Next() {
 		var u User
 		err = rows.Scan(&u.Id, &u.Name, &u.Surname, &u.Age)
 		errorHandle(err, "Failed to read the row values")
+		// Append the User Struct to the response array.
 		resp = append(resp, structToMap(u))
 	}
 	return resp
