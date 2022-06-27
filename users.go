c82e76095c097a3e8dda815cd587277296034f65
WiaanB <whoppingwaki@gmail.com>
2022-06-27 19:44:53 +0200

    sql statement of the values for the user

--- users.go
+++ users.go
@@ -29,8 +29,7 @@ func (u *User) validateUser() []string {
 }
 
 func (u *User) sqlStatement() string {
-	var s string
-	return s
+	return fmt.Sprintf("(Name, Surname, Age) VALUES (%s, %s, %d)", u.Name, u.Surname, u.Age)
 }
 
 func handleUsers(w http.ResponseWriter, req *http.Request) {
