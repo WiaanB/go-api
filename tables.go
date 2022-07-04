6bf84c60c47241c510ef969adf86f3f1bd93b816
WiaanB <whoppingwaki@gmail.com>
2022-07-04 21:39:58 +0200

    removed other tables, and focussed on a user API

--- tables.go
+++ tables.go
@@ -12,27 +12,6 @@ func initializeTables() {
 	`
 	executeSQL(users)
 	executeSQL("INSERT INTO users (name, surname, age) VALUES ('wiaan', 'botha', 23);")
-	books := `DROP TABLE IF EXISTS books;
-	CREATE TABLE books (
-		id 			SERIAL PRIMARY KEY,
-		ISBN		INT UNIQUE,
-		title		TEXT NOT NULL,
-		author		TEXT NOT NULL,
-		pages		INT
-	);
-	`
-	executeSQL(books)
-	executeSQL("INSERT INTO books (ISBN, title, author, pages) VALUES (2341, 'The Martian Threat', 'Dave Campbell', 342);")
-	authors := `DROP TABLE IF EXISTS authors;
-	CREATE TABLE authors (
-		id 			SERIAL PRIMARY KEY,
-		name		TEXT NOT NULL,
-		books		INT,
-		awards		INT
-	);
-	`
-	executeSQL(authors)
-	executeSQL("INSERT INTO authors (name, books, awards) VALUES ('Dave Campbell', 1, 0);")
 }
 
 func executeSQL(query string) {
